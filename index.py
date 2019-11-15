2dcadd14046a67f24bfa22241c892d9cc542beae
Vanek_on_mac <ivan.cheprasov@phystech.edu>
2019-11-15 22:48:30 +0200

    initial test flask

---
+++ index.py
@@ -0,0 +1,12 @@
+from flask import *
+import pandas as pd
+import requests, json
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+	return render_template('home.html')
+
+if __name__=='__main__':
+	app.run()
